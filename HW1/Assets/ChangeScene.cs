ec9b921edd26e8cc8f103af5259ae32692fa2df4
Alex Lopez <alope6@uic.edu>
2019-09-14 03:26:22 -0500

    added button, still buggy if book moves

---
+++ HW1/Assets/ChangeScene.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ChangeScene : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public Boolean flag;
+    public GameObject vbBtnObj;
+
+    // Use this for initialization
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("Button");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        flag = true;
+        Debug.Log("Button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        if(flag)
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+
+        Debug.Log("Button released");
+    }
+}
